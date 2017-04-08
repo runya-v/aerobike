ffb926ab7e4f238a31d45ef0b3b973151dbe13f6
Ростислав Величко <rostislav.vel@gmail.com>
2017-04-08 11:42:42 +0300

    Включена прозрачность во вьювере.

---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+/*!
+ * \brief  Модуль контроллеров для игры "Летающий мотоцикл".
+ * \author Rostislav Velichko. e: rostislav.vel@gmail.com
+ * \date   04.04.2017
+ */
+
+var CONTROLLERS = {};
+
+
+CONTROLLERS.BikeCameraControls = function(object, terrain, domElement) {
+
+};
+CONTROLLERS.BikeCameraControls.prototype = Object.create(THREE.EventDispatcher.prototype);
