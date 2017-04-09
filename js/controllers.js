06bc760e6a534cc3d07a0da0df06b6e2b25097c0
Ростислав Величко <rostislav.vel@gmail.com>
2017-04-09 21:21:33 +0300

    Добавлена сущность контролеры.

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,30 @@
 var CONTROLLERS = {};
 
 
-CONTROLLERS.BikeCameraControls = function(object, terrain, domElement) {
+CONTROLLERS.BikeControls = function(Bike, terrain, domElement) {
+    var EPS = 0.000001;
+    var PIXELS_PER_ROUND = 1800;
 
+    this.keys = {
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        BOTTOM: 40
+    };
+
+    this.object = object;
+    this.domElement = ( domElement !== undefined ) ? domElement : document;
+
+    this.center = new THREE.Vector3();
+
+    this.minPolarAngle = 0; // radians
+    this.maxPolarAngle = Math.PI; // radians
+    this.minDistance = 0;
+    this.maxDistance = Infinity;
+
+    var _self = this;
+    var _last_position = new THREE.Vector3();
+
+    
 };
-CONTROLLERS.BikeCameraControls.prototype = Object.create(THREE.EventDispatcher.prototype);
+CONTROLLERS.BikeControls.prototype = Object.create(THREE.EventDispatcher.prototype);
